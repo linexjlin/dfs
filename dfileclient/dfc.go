5f52d2bb83dbfe4ac7455903dadd6ecfd9a7aec1
lin xj <lin_x_000@MIKIPC.(none)>
2015-04-06 21:03:12 +0800

    origin version

---
+++ dfileclient/dfc.go
@@ -0,0 +1,133 @@
+// dfileclient project main.go
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+var basedir, remoteAddr, stype, estring string
+
+//写文件到网络
+func fileToTcp(w io.Writer, r io.Reader) {
+	io.Copy(w, r)
+}
+
+//判断文件是否存在
+func fExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+//创建一个连接，如果连接不上，等待3秒后重试
+func getConn() net.Conn {
+	var c net.Conn
+	var e error
+	for {
+		c, e = net.Dial("tcp", remoteAddr)
+		if e != nil {
+			lg.Println("can't not dial. wait 3 second to retry...")
+			time.Sleep(time.Second * 3)
+		} else {
+			lg.Println("new connection estabinished")
+			break
+		}
+	}
+	return c
+}
+
+//转发送过来的文件进行处理，返回处理过的文件名
+func fileProcess(infile string) string {
+	outfile := infile
+
+	exes := strings.Replace(estring, `#i#`, infile, -1) //#i#替换成真正文件名
+	cmd := new(exec.Cmd)
+	tslice := strings.Split(exes, " ")
+	cmd.Path = tslice[0]
+	cmd.Args = tslice
+	lg.Println("The command running: ", cmd.Args)
+	_, e := cmd.Output()
+	if e != nil {
+		lg.Println("execute os command error: ", e)
+	} else {
+		lg.Println("outfile will be change to:", tslice[len(tslice)-1])
+		outfile = tslice[len(tslice)-1]
+	}
+	return outfile
+}
+
+//根据客户配置的类型，判断是否需要处理，-e 参数可以指定处理的方法。
+func openFile(fn string) *os.File {
+	var f *os.File
+
+	switch stype {
+	case "audio":
+		lg.Println("audio type processing...")
+		newName := fileProcess(fn)
+		f, _ = os.Open(newName)
+	case "any":
+		f, _ = os.Open(fn)
+	default:
+		f, _ = os.Open(fn)
+	}
+	return f
+}
+
+//处理接到的文件名，查找请求并反馈
+func clientConn(needNewChan chan bool) {
+	c := getConn()
+	reader := bufio.NewReader(c)
+
+	fndata, _, _ := reader.ReadLine()
+	needNewChan <- true //to make next connection
+	os.Chdir(basedir)
+	filename := basedir + string(fndata)
+	lg.Println("the file name is:", filename)
+
+	if !fExist(filename) {
+		lg.Println("the file can't find, socket close")
+		c.Write([]byte("0"))
+		c.Close()
+	} else {
+		lg.Println("found file!")
+
+		c.Write([]byte("1"))
+		lg.Println("write file to tcp!")
+		f := openFile(filename)
+		fileToTcp(c, f)
+		f.Close()
+		lg.Println("socket close")
+		c.Close()
+	}
+}
+
+var lg = log.New(os.Stdout, " ", log.LstdFlags|log.Llongfile)
+var serveType string
+
+//处理命令行参数
+func parseFlag() {
+	flag.StringVar(&basedir, "b", `.`, "-b = \"/home\"")
+	flag.StringVar(&remoteAddr, "r", `127.0.0.1:8612`, "-host=\"127.0.0.1:8612\"")
+	flag.StringVar(&stype, "t", "any", "-t=\"audio\"  \"Support audio only\"")
+	flag.StringVar(&estring, "e", " ", `sox.exe #i# #i#.mp3,where "#i# is the variable will replace with the file`)
+	flag.Parse()
+}
+
+func main() {
+	parseFlag()
+	needNewChan := make(chan bool)
+	go clientConn(needNewChan)
+	for {
+		select {
+		case <-needNewChan:
+			go clientConn(needNewChan)
+		}
+	}
+}
---
+++ dfileserve/dfs.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var sockets = make([]net.Conn, 0)
+var sLock sync.Mutex
+
+func tcpFileToHTTP(httpWriter io.Writer, fileReceiver io.Reader) error {
+	_, e := io.Copy(httpWriter, fileReceiver)
+	return e
+}
+
+func getFileName(fn string) string {
+	return fn
+}
+
+//根据返回的reader 判断是否找到了文件
+func findFile(fn string) (io.Reader, bool) {
+	found := false
+	if len(sockets) == 0 {
+		lg.Println("no client connected!")
+		return nil, found
+	}
+
+	fr := getFileReader(fn)
+
+	if fr != nil {
+		lg.Println("found file")
+		found = true
+		return fr, found
+	}
+	lg.Println("no found")
+	return nil, found
+}
+
+//扫描客户端判断看看有没有结果
+func getFileReader(fn string) io.Reader {
+	//lock to copy current sockets.
+	sLock.Lock()
+	tmpSockets := sockets
+	sLock.Unlock()
+
+	c := make(chan net.Conn) //通过chan 接收返回的连接
+	cnt := make(chan int, 1) //返回的结果计数器
+	maxcnt := len(tmpSockets)
+	lg.Println("the length of tmp sockets is :", maxcnt)
+
+	for _, s := range tmpSockets {
+		go sendNameGetReader(s, fn, c, cnt)
+	}
+
+	//等待所有socket 结束，如果没找到则返一个nil 给filereader
+	j := 0
+	go func() {
+		for {
+			<-cnt
+			j++
+			lg.Println("get ", j, " of ", maxcnt)
+			//			maxcnt--
+			if j >= maxcnt {
+				lg.Println("scan sockets finished!")
+				c <- nil //返回nil 给filereader
+				break
+			}
+		}
+	}()
+
+	//等待客户端给出结果，可能是找到(get from ↑)，也可能是全都找不到这个文件(get from go routine)
+	lg.Println("wait chan ...")
+	var fr net.Conn //file reader
+	fr = nil
+	select {
+	case fr = <-c: //这里的filerader 可能得到两个值，一个是找到了文件后返回的正确reader， 另一个是所有遍历所有socket后还是找不到，返回nil， 这个nil在上一层调用会被判为找不到。
+		//lg.Println("receive:")
+	}
+
+	//将用掉的socket 删除掉
+	sLock.Lock()
+	lg.Println("befor truncate length is :", len(sockets))
+	sockets = sockets[maxcnt:]
+	lg.Println("after truncate length is", len(sockets))
+	sLock.Unlock()
+
+	return fr
+}
+
+//发送文件名，并将结果通过 Conn chan跟 int chan 发给上级调用函数
+func sendNameGetReader(s net.Conn, fn string, c chan net.Conn, cnt chan int) error {
+	s.Write([]byte(fn + "\r\n")) //
+	b := make([]byte, 1)
+	lg.Println("wait to read one byte from socket...")
+
+	//tcp 读是阻塞的
+	for {
+		n, _ := s.Read(b)
+		lg.Println("receive:", n, "bytes")
+		lg.Println("the byte is", string(b))
+
+		//收到客户端的字符0表示没找到文件
+		if string(b) == "0" {
+			lg.Println("file no found")
+			cnt <- 1
+			break
+		}
+
+		if n > 0 {
+			c <- s
+			cnt <- 1
+			break
+		} else { //收不到客户端的结果了表示收不到
+			cnt <- 1
+			s.Close()
+			break
+		}
+	}
+	lg.Println("go routine,finish")
+	return nil
+}
+
+//处理发送过来的请求
+func ServeAll(rw http.ResponseWriter, r *http.Request) {
+	lg.Println("ServeAll function")
+	fileName := getFileName(r.URL.Path)
+	fileReceiver, found := findFile(fileName)
+	if found {
+		tcpFileToHTTP(rw, fileReceiver)
+	} else {
+		http.NotFound(rw, r)
+	}
+}
+
+var lg = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Llongfile)
+
+func main() {
+	l, _ := net.Listen("tcp", ":8612")
+	go func() {
+		for {
+			s, _ := l.Accept()
+			defer s.Close()
+			sLock.Lock()
+			sockets = append(sockets, s)
+			lg.Println("New socket coming, current sockets length:", len(sockets))
+			sLock.Unlock()
+
+		}
+	}()
+
+	h := http.HandlerFunc(ServeAll)
+	http.ListenAndServe(":8701", h)
+}
